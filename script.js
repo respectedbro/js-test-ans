ab3054b614b90101f62258a28bc6e93efdffe91c
resAsus <respectedbro@yandex.ru>
2025-05-08 17:24:31 +0300

    dis inputs, add right ans

--- script.js
+++ script.js
@@ -15,138 +15,148 @@ let correctAnswer = 0;
 let incorrectAnswer = 0;
 
 const showElems = (elem) => {
-  startBtn.style.display = 'none';
-  questions.style.display = 'none';
-  nextBtn.style.display = 'none';
-  results.style.display = 'none';
-  againBtn.style.display = 'none';
+    startBtn.style.display = 'none';
+    questions.style.display = 'none';
+    nextBtn.style.display = 'none';
+    results.style.display = 'none';
+    againBtn.style.display = 'none';
 
-  if (elem.includes('startBtn')) startBtn.style.display = 'block';
-  if (elem.includes('questions')) questions.style.display = 'flex';
-  if (elem.includes('nextBtn')) nextBtn.style.display = 'block';
-  if (elem.includes('results')) results.style.display = 'flex';
-  if (elem.includes('againBtn')) againBtn.style.display = 'block';
+    if (elem.includes('startBtn')) startBtn.style.display = 'block';
+    if (elem.includes('questions')) questions.style.display = 'flex';
+    if (elem.includes('nextBtn')) nextBtn.style.display = 'block';
+    if (elem.includes('results')) results.style.display = 'flex';
+    if (elem.includes('againBtn')) againBtn.style.display = 'block';
 };
 
 const startQuestions = () => {
-  questionElements.forEach(question => {
-    question.style.display = 'none';
-  });
+    questionElements.forEach(question => {
+        question.style.display = 'none';
+    });
 };
 
 startQuestions();
 showElems(['startBtn']);
 
 startBtn.addEventListener('click', () => {
-  startQuestions();
-  showElems(['questions']);
-  questionElements[0].style.display = 'flex';
-  questionsCountElement.textContent = '1';
+    startQuestions();
+    showElems(['questions']);
+    questionElements[0].style.display = 'flex';
+    questionsCountElement.textContent = '1';
 });
 
 questionElements.forEach(question => {
-  const inputAnswers = document.querySelectorAll('.question__input');
-  const answers = question.querySelectorAll('.questions__item button');
+    const inputAnswers = document.querySelectorAll('.question__input');
+    const answers = question.querySelectorAll('.questions__item button');
 
-  answers.forEach(answer => {
-    answer.addEventListener('click', () => {
-      if (answerSelected) {
-        return;
-      }
-      answerSelected = true;
+    answers.forEach(answer => {
+        answer.addEventListener('click', () => {
+            if (answerSelected) {
+                return;
+            }
+            answerSelected = true;
 
-      answers.forEach(btn => {
-        btn.style.pointerEvents = 'none';
-      });
+            answers.forEach(btn => {
+                btn.style.pointerEvents = 'none';
+            });
 
-      if (answer.classList.contains('right-answer')) {
-        answer.classList.add('true');
-        correctAnswer++;
-        correctAnswerElement.textContent = correctAnswer;
-      } else {
-        answer.classList.add('false');
-        incorrectAnswer++;
-        incorrectAnswerElement.textContent = incorrectAnswer;
+            if (answer.classList.contains('right-answer')) {
+                answer.classList.add('true');
+                correctAnswer++;
+                correctAnswerElement.textContent = correctAnswer;
+            } else {
+                answer.classList.add('false');
+                incorrectAnswer++;
+                incorrectAnswerElement.textContent = incorrectAnswer;
 
-        const correctAnswerBtn = question.querySelector('.right-answer');
-        correctAnswerBtn.classList.add('true');
-      }
+                const correctAnswerBtn = question.querySelector('.right-answer');
+                correctAnswerBtn.classList.add('true');
+            }
 
-      showElems(['questions', 'nextBtn']);
+            showElems(['questions', 'nextBtn']);
+        });
     });
-  });
 
-  inputAnswers.forEach((input) => {
-    input.addEventListener('keypress', (e) => {
-      if (e.key === 'Enter' && !answerSelected) {
-        answerSelected = true;
-        // input.readOnly = true;
+    inputAnswers.forEach((input) => {
+        const answerContainer = input.closest('.questions__elem');
+        const correctAnswerElement = document.createElement('div');
+        correctAnswerElement.className = 'correct-answer-text';
 
-        const corrAnswer = input.dataset.correct;
-        const userAnswer = input.value.toLowerCase().trim();
+        input.addEventListener('keypress', (e) => {
 
-        if (userAnswer.toLowerCase() === corrAnswer.toLowerCase()) {
-          input.classList.add('true');
-          correctAnswer++;
-          correctAnswerElement.textContent = correctAnswer;
-        } else {
-          input.classList.add('false');
-          incorrectAnswer++;
-          incorrectAnswerElement.textContent = incorrectAnswer;
-        }
+            if (e.key === 'Enter' && !answerSelected) {
+                answerSelected = true;
+                input.disabled = true;
 
-        showElems(['questions', 'nextBtn']);
-      }
+                const corrAnswer = input.dataset.correct;
+                const userAnswer = input.value.toLowerCase().trim();
+
+                if (userAnswer.toLowerCase() === corrAnswer.toLowerCase()) {
+                    input.classList.add('true');
+                    correctAnswer++;
+                    correctAnswerElement.textContent = correctAnswer;
+                } else {
+                    input.classList.add('false');
+                    incorrectAnswer++;
+                    incorrectAnswerElement.textContent = incorrectAnswer;
+
+                    correctAnswerElement.textContent = `Правильный ответ: ${corrAnswer}`;
+                    answerContainer.appendChild(correctAnswerElement);
+                }
+
+                showElems(['questions', 'nextBtn']);
+            }
+        });
     });
-  });
 });
 
 nextBtn.addEventListener('click', () => {
-  answerSelected = false;
-  questionElements[questionIndex].style.display = 'none';
-  questionIndex++;
+    answerSelected = false;
+    questionElements[questionIndex].style.display = 'none';
+    questionIndex++;
 
-  if (questionIndex < questionElements.length) {
+    if (questionIndex < questionElements.length) {
+        document.querySelectorAll('.correct-answer-text').forEach((el) => el.remove());
 
-    questionElements[questionIndex].style.display = 'flex';
-    questionsCountElement.textContent = questionIndex + 1;
-    showElems(['questions']);
+        questionElements[questionIndex].style.display = 'flex';
+        questionsCountElement.textContent = questionIndex + 1;
+        showElems(['questions']);
 
-    if (questionIndex === questionElements.length - 1) {
-      nextBtn.textContent = 'Посмотреть результаты';
+        if (questionIndex === questionElements.length - 1) {
+            nextBtn.textContent = 'Посмотреть результаты';
+        }
+    } else {
+        showElems(['results', 'againBtn']);
     }
-  } else {
-    showElems(['results', 'againBtn']);
-  }
 });
 
 againBtn.addEventListener('click', () => {
-  questionIndex = 0;
-  correctAnswer = 0;
-  incorrectAnswer = 0;
-  correctAnswerElement.textContent = '0';
-  incorrectAnswerElement.textContent = '0';
-  answerSelected = false;
-  nextBtn.textContent = 'Следующий вопрос';
+    questionIndex = 0;
+    correctAnswer = 0;
+    incorrectAnswer = 0;
+    correctAnswerElement.textContent = '0';
+    incorrectAnswerElement.textContent = '0';
+    answerSelected = false;
+    nextBtn.textContent = 'Следующий вопрос';
 
-  questionElements.forEach(question => {
-    question.style.display = 'none';
-    const buttons = question.querySelectorAll('button');
-    buttons.forEach(btn => {
-      btn.classList.remove('true', 'false');
-      btn.style.pointerEvents = 'auto';
+    questionElements.forEach(question => {
+        question.style.display = 'none';
+        const buttons = question.querySelectorAll('button');
+        buttons.forEach(btn => {
+            btn.classList.remove('true', 'false');
+            btn.style.pointerEvents = 'auto';
+        });
+
+        const inputs = question.querySelectorAll('.question__input');
+        inputs.forEach(input => {
+            input.classList.remove('true', 'false');
+            input.value = '';
+            input.disabled = false;
+        });
     });
 
-    const inputs = question.querySelectorAll('.question__input');
-    inputs.forEach(input => {
-      input.classList.remove('true', 'false');
-      input.value = '';
-    });
-  });
-
-  showElems(['questions']);
-  questionElements[0].style.display = 'flex';
-  questionsCountElement.textContent = '1';
+    showElems(['questions']);
+    questionElements[0].style.display = 'flex';
+    questionsCountElement.textContent = '1';
 });
 
 
